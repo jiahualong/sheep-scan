0f5cd8543fe97ce4cea29a537e55536ef8e294fe
JiaHualong <magic.hualong@gmail.com>
2016-06-14 16:50:22 +0800

    可获取网页第一个img的src

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,46 @@
-document.addEventListener('DOMContentLoaded', function (){
-    document.querySelector('#check').addEventListener('click', function () {
-        document.querySelector('#result').innerHTML += "hello<br />"
-    })
+/** 检测图片 */
+function check() {
+
+    // var s = chrome.extension.getBackgroundPage();
+    // document.querySelector('#result').innerHTML = s;
+
+    // chrome.tabs.executeScript(null, {code:"document.body.style.backgroundColor='red'"});
+
+    // chrome.tabs.executeScript(null,
+    //     {code:"document.querySelector('img')"});
+
+    // chrome.tabs.executeScript(null, {code: "var x = 10; x"},
+    //     function (results) {
+    //         document.querySelector('#result').innerHTML = results;
+    // });
+
+    chrome.tabs.executeScript(null, {code: " document.querySelector('img').getAttribute('src')"},
+        function (results) {
+            document.querySelector('#result').innerHTML = results;
+    });
+
+
+    // document.querySelector('#result').innerHTML = "abc";
+
+    // document.querySelector("img").innerHTML ="123";
+
+    // for (i = 0; i < imgs.length; i++) {
+    // document.querySelector('#result').innerHTML += imgs[i].getAttribute('src') + "<br />";
+    // document.querySelector('#result').innerHTML += "hello<br />"
+    // }
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#check').addEventListener('click', check);
+    /*
+     chrome.browserAction.onClicked.addListener(function (tab) {
+     chrome.tabs.executeScript(null,
+     {code: "document.body.bgColor='red'"});
+     });
+     */
+
 });
+
+
