33db8e4f6595b78f4395f37ae67d7a2e2cc5fff4
JiaHualong <magic.hualong@gmail.com>
2016-06-15 15:25:48 +0800

    返回json

--- js/getimg.js
+++ js/getimg.js
@@ -1,6 +1,12 @@
 var imgs = document.querySelectorAll('img');
-var urls = "";
+var urls = {};
 for (i = 0; i < imgs.length; i++) {
-    urls += imgs[i].getAttribute('src') + "<br />";
+
+    if (urls[imgs[i].getAttribute('src')]) {
+        urls[imgs[i].getAttribute('src')] += 1;
+    } else {
+        urls[imgs[i].getAttribute('src')] = 1;
+    }
+
 }
-urls
+JSON.stringify(urls)
